1547804a4fa4bcbc769c30b9d810365b6cad1fc6
kanyi <jkanyi757@gmail.com>
2024-12-10 11:38:36 +0300

    small additions

--- users/signals.py
+++ users/signals.py
@@ -1,13 +1,18 @@
+import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import UserProfile
 
+logger = logging.getLogger(__name__)
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+        logger.info(f"UserProfile created for user {instance.username}")
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+    logger.info(f"UserProfile saved for user {instance.username}")
